2604062c27b0139c010e336929b75c303081e5c9
wael <78425538+wa20@users.noreply.github.com>
2021-06-13 11:46:01 +0100

    package j son updated and initial connection js updated

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const helpers=require("./utils/helpers")
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const fileUpload = require("express-fileupload");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({helpers});
+
+const sessionOptions = {
+  secret: "super secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// app.use(bodyParser.json())
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(session(sessionOptions))
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}.`));
+});
