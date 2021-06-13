5f2c2a824536b4fb998ef4c081ef6c5e76b84254
wael <78425538+wa20@users.noreply.github.com>
2021-06-13 23:51:38 +0100

    server code updates and added helpers file js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-
-// const helpers=require("./utils/helpers")
-const routes = require("./controllers/index");
+const helpers = require("./utils/helpers");
+const routes = require("./controllers");
 const sequelize = require("./config/connections");
-const hbs = exphbs.create({});
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(routes);
 
