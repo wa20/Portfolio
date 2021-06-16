0d535ba702092a282cdddd1df69ed5529d702207
wael <78425538+wa20@users.noreply.github.com>
2021-06-16 15:23:02 +0100

    navbar colour transition on scroll added, not working?

---
+++ public/js/navbar.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+// const  myNav = document.getElementById('navBar');
+// window.addEventListener('scroll', (event) =>  { 
+//     "use strict";
+//     { capture: true }
+//     if (window.scrollY > 20 ) {
+//         myNav.classList.add("nav-dark");
+//     } 
+//     else {   
+//         myNav.classList.remove("nav-dark");
+//     }
+
+// });
+
+window.addEventListener('scroll', ()=> {
+  console.log('scrolled')
+})
+
